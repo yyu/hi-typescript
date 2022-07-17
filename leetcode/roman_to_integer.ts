bacf55e3a45fcf682d01f5a0bb2a1794d5ba662c
y² <yy@yuyuan.org>
2022-07-16 20:30:48 -0700

    leetcode - Roman to Integer (v2)

--- leetcode/roman_to_integer.ts
+++ leetcode/roman_to_integer.ts
@@ -1,4 +1,4 @@
-function romanToInt(s: string): number {
+function romanToInt0(s: string): number {
     let roman_tables: {[key:string]:number}[] = [
         {},
         {
@@ -53,6 +53,76 @@ function romanToInt(s: string): number {
                 for (const roman_digits in r2i) {
                     if (subs === roman_digits) {
                         result += r2i[roman_digits];
+                        return i + nr_digits;
+                    }
+                }
+            }
+        }
+        throw new Error("should never reach here");
+    }
+
+    while (i < len) {
+        i = greedy_find();
+    }
+
+    return result;
+};
+
+function romanToInt(s: string): number {
+    let roman_tables: Map<string, number>[] = [
+        new Map(),
+        new Map([
+            ["I", 1],
+            ["V", 5],
+            ["X", 10],
+            ["L", 50],
+            ["C", 100],
+            ["D", 500],
+            ["M", 1000],
+        ]),
+        new Map([
+            ["II", 2],
+            ["XX", 20],
+            ["CC", 200],
+            ["MM", 2000],
+            ["IV", 4],
+            ["XL", 40],
+            ["CD", 400],
+            ["VI", 6],
+            ["LX", 60],
+            ["DC", 600],
+            ["IX", 9],
+            ["XC", 90],
+            ["CM", 900],
+        ]),
+        new Map([
+            ["III", 3],
+            ["XXX", 30],
+            ["CCC", 300],
+            ["MMM", 3000],
+            ["VII", 7],
+            ["LXX", 70],
+            ["DCC", 700],
+        ]),
+        new Map([
+            ["VIII", 8],
+            ["LXXX", 80],
+            ["DCCC", 800],
+        ]),
+    ];
+
+    let result = 0;
+    let len = s.length;
+    let i = 0;
+
+    function greedy_find(): number {
+        for (let nr_digits = 4; nr_digits > 0; nr_digits--) {
+            if (i + nr_digits <= len) {
+                let subs = s.substring(i, i + nr_digits);
+                let r2i = roman_tables[nr_digits];
+                for (const [roman, arabic] of r2i) {
+                    if (subs === roman) {
+                        result += arabic;
                         return i + nr_digits;
                     }
                 }
